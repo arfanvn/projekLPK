6c9fb9684dd629aa26138490658754a7553f04ca
arfanvn <138582276+arfanvn@users.noreply.github.com>
2024-07-09 10:31:07 +0700

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -4,12 +4,7 @@ import { Image, Text, TextInput, View } from 'react-native';
 export default class App extends Component {
   render() {
     return (
-      <View>
-        <Text style={{}}> coba dulu </Text>
-        <Text>anjayyy</Text>
-        <TextInput />
-        <Image/>
-      </View>
+      
     );
   }
 }
