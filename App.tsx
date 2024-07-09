571757b5618e10527b879c632471ed02c674304b
fajar3 <69444231+fajar3@users.noreply.github.com>
2024-07-09 10:25:46 +0700

    p
    lorem
    
    Co-Authored-By: arfanvn <138582276+arfanvn@users.noreply.github.com>
    Co-Authored-By: eceransurya <99064079+eceransurya@users.noreply.github.com>

---
+++ App.tsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { Image, Text, TextInput, View } from 'react-native';
+
+export default class App extends Component {
+  render() {
+    return (
+      <View>
+        <Text style={{}}> coba dulu </Text>
+        <Text>anjayyy</Text>
+        <TextInput />
+        <Image/>
+      </View>
+    );
+  }
+}
